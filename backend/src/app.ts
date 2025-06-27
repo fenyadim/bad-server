6a44ac56da98962a519eeb16d54d1ce13cbf02da
Dima <fenyadim>
2025-06-27 22:03:03 +0800

    Первая попытка

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,14 @@
 import { errors } from 'celebrate'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import csrf from 'csurf'
 import 'dotenv/config'
 import express, { json, urlencoded } from 'express'
 import mongoose from 'mongoose'
 import path from 'path'
 import { DB_ADDRESS } from './config'
 import errorHandler from './middlewares/error-handler'
+import limiter from './middlewares/limiter'
 import serveStatic from './middlewares/serverStatic'
 import routes from './routes'
 
@@ -14,10 +16,13 @@ const { PORT = 3000 } = process.env
 const app = express()
 
 app.use(cookieParser())
+const csrfProtection = csrf({ cookie: true })
 
 app.use(cors())
 // app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(limiter)
 
 app.use(serveStatic(path.join(__dirname, 'public')))
 
@@ -28,6 +33,10 @@ app.options('*', cors())
 app.use(routes)
 app.use(errors())
 app.use(errorHandler)
+
+app.get('/csrf-token', csrfProtection, (req, res) => {
+    res.send(req.csrfToken())
+})
 
 // eslint-disable-next-line no-console
 
