324c481152b70017c64faebcc560c21903e24a03
fenyadim <dimos9813@gmail.com>
2025-07-02 15:46:53 +0800

    Первые правки

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import { errors } from 'celebrate'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import { doubleCsrf } from 'csrf-csrf'
 import 'dotenv/config'
 import express, { json, urlencoded } from 'express'
+import rateLimit from 'express-rate-limit'
 import mongoose from 'mongoose'
 import path from 'path'
-import { DB_ADDRESS } from './config'
+import { DB_ADDRESS, ORIGIN_ALLOW } from './config'
 import errorHandler from './middlewares/error-handler'
 import serveStatic from './middlewares/serverStatic'
 import routes from './routes'
@@ -15,19 +17,36 @@ const app = express()
 
 app.use(cookieParser())
 
-app.use(cors())
-// app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+const { doubleCsrfProtection, generateCsrfToken } = doubleCsrf({
+    getSecret: () => 'your-very-secret-key',
+    getSessionIdentifier: (req) => req.ip || '',
+})
+
+app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }))
 
 app.use(serveStatic(path.join(__dirname, 'public')))
 
 app.use(urlencoded({ extended: true }))
 app.use(json())
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 30,
+    message: 'Too many requests, please try again later.',
+})
+
+app.use(limiter)
 app.options('*', cors())
 app.use(routes)
 app.use(errors())
 app.use(errorHandler)
+
+app.get('/csrf-token', (req, res) => {
+    const csrfToken = generateCsrfToken(req, res)
+    res.send({ csrfToken })
+})
+
+app.use(doubleCsrfProtection)
 
 // eslint-disable-next-line no-console
 
